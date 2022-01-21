d7744caeb4abd8e7819a35a348196097c0901b7c
zgm <zgm@qq.com>
2022-01-21 11:33:07 +0800

    id=2018320118 name=zgm age=22

---
+++ src/com/zgm/domain/Student.java
@@ -0,0 +1,15 @@
+package com.zgm.domain;
+
+/**
+ * ClassName:Student
+ * package:com.zgm.domain
+ * Description:
+ *
+ * @Date:2022/1/21 0021 11:31
+ * @Author:zgm3331529718@qq.com
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+}
