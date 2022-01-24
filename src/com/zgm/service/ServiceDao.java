4c4165bda491f097c9d8c0531af5d733b0860893
zgm <zgm@qq.com>
2022-01-24 18:08:27 +0800

    my GitHubFile

---
+++ src/com/zgm/service/ServiceDao.java
@@ -0,0 +1,12 @@
+package com.zgm.service;
+
+/**
+ * ClassName:ServiceDao
+ *
+ * @Description: 删库跑路
+ * @Date: 2022/1/24 0024 18:06
+ * @Author: zgm 3331529718@qq.com
+ */
+public interface ServiceDao {
+    int insertUser();
+}
---
+++ src/com/zgm/service/ServiceDaoImpl.java
@@ -0,0 +1,15 @@
+package com.zgm.service;
+
+/**
+ * ClassName:ServiceDaoImpl
+ *
+ * @Description: 删库跑路
+ * @Date: 2022/1/24 0024 18:07
+ * @Author: zgm 3331529718@qq.com
+ */
+public class ServiceDaoImpl implements ServiceDao {
+    @Override
+    public int insertUser() {
+        return 0;
+    }
+}
